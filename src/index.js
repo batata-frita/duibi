052027b871204a588b6ed3cf8d3c333e2a881940
Kir Belevich <kir.belevich@klarna.com>
2017-11-09 11:59:31 +0100

    Parse screenshot buffer as a pixels data structure 🖼

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const React = require('react')
-const temp = require('temp')
-const fs = require('fs')
-const puppeteer = require('puppeteer')
-const ReactDOMServer = require('react-dom/server')
-const Button = require('@klarna/bubble-ui').Button
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import temp from 'temp'
+import fs from 'fs'
+import puppeteer from 'puppeteer'
+import { PNG } from 'pngjs'
+import { promisify } from 'util'
 
 // Automatically track and cleanup files at exit
 temp.track()
@@ -13,7 +14,9 @@ const content = `
     body { margin: 0 }
   </style>
   <body>
-    ${ReactDOMServer.renderToString(<Button.Primary>Regular</Button.Primary>)}
+    ${ReactDOMServer.renderToString(
+      <button style={{ backgroundColor: 'red' }}>Regular</button>
+    )}
   </body>
 `
 const htmlFilename = temp.path({ suffix: '.html' })
@@ -31,6 +34,13 @@ const doIt = async () => {
 
   const element = await chrome.$('button')
   const screenshot = await element.screenshot()
+
+  const png = new PNG({ filterType: 4 })
+  const parse = promisify(png.parse.bind(png))
+
+  const parsedData = await parse(screenshot)
+
+  console.log(parsedData.data)
 
   await chrome.close()
 
