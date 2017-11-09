d9d3811d6e462034dc07b0cbd175bc42038f7f90
Kir Belevich <kir.belevich@klarna.com>
2017-11-09 16:00:39 +0100

    💄

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import meow from 'meow'
+import chalk from 'chalk'
+import termImg from 'term-img';
 import duibi from '.'
 
 const cli = meow(`
@@ -15,7 +17,24 @@ duibi({
 	containerSelector: cli.input[1],
 	textSelector: cli.input[2]
 })
-.then(console.log)
+.then((result) => {
+	console.log(chalk.magenta('Background'))
+	termImg(result.backgroundScreenshotPath, () => {})
+	console.log(chalk.cyan('• screenshot:'), result.backgroundScreenshotPath)
+	console.log(chalk.cyan('• average color:'), result.backgroundAverageColor)
+	console.log(chalk.magenta('\nText'))
+	termImg(result.containerScreenshotPath, () => {})
+	console.log(chalk.cyan('• screenshot:'), result.containerScreenshotPath)
+	console.log(chalk.cyan('• color:'), result.foregroundColor)
+	console.log(chalk.magenta('\nReport'))
+	console.log(chalk.cyan('• color contrast ratio:'), result.ratio)
+	console.log(chalk.cyan('• a11y score:'), result.score)
+	console.log(chalk.cyan('• is accessible:'), result.isAccessible ? '✅' : '❌')
+
+	if (!result.isAccessible) {
+		process.exit(1)
+	}
+})
 .catch((error) => {
 	console.error(error)
 	process.exit(1)
