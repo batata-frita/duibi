2fd1f3a605983856f0f5fb973a5602ec9ce9bb00
Paulo Ragonha <paulo@ragonha.me>
2017-11-09 11:31:26 +0100

    Replace `navalia` with `puppeteer` 🐶

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const React = require('react')
 const temp = require('temp')
 const fs = require('fs')
-const { Chrome } = require('navalia')
+const puppeteer = require('puppeteer')
 const ReactDOMServer = require('react-dom/server')
 const Button = require('@klarna/bubble-ui').Button
 
 // Automatically track and cleanup files at exit
 temp.track()
 
-const content = ReactDOMServer.renderToString(<Button.Primary>Regular</Button.Primary>)
+const content = `
+  <style>
+    body { margin: 0 }
+  </style>
+  <body>
+    ${ReactDOMServer.renderToString(<Button.Primary>Regular</Button.Primary>)}
+  </body>
+`
 const htmlFilename = temp.path({ suffix: '.html' })
 const imageFilename = temp.path({ suffix: '.png' })
 const url = `file://${htmlFilename}`
@@ -18,13 +25,14 @@ const doIt = async () => {
 
   fs.writeFileSync(htmlFilename, content)
 
-  const chrome = new Chrome()
+  const browser = await puppeteer.launch()
+  const chrome = await browser.newPage()
   await chrome.goto(url)
 
-  const screenshot = await chrome.screenshot('body')
+  const element = await chrome.$('button')
+  const screenshot = await element.screenshot()
 
-  await chrome.done()
-  await chrome.kill()
+  await chrome.close()
 
   console.log('image', imageFilename)
   fs.writeFileSync(imageFilename, screenshot)
